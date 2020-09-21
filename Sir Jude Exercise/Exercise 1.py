4a74d5af2b782f9d9e9ca7f8cc5101c79c7a7df6
Bernard Wijaya <71361577+bernardwija@users.noreply.github.com>
2020-09-21 13:29:22 +0700

    Add files via upload

---
+++ Sir Jude Exercise/Exercise 1.py
@@ -0,0 +1,5 @@
+print('Hello, Welcome to the Radian Counter')
+degrees = input('Please input your angle here : ')
+radian = float(degrees) * 3.14 / 180
+print('Your radian is', radian)
+print('Thankyou for using Radian Converter')
---
+++ Sir Jude Exercise/Exercise 4.py
@@ -0,0 +1,7 @@
+print('Hello, Welcome to Circumference Counter')
+piediameter = float(input('Please input your diameter here: '))
+pi = 3.14
+pieradius = piediameter // 2
+circumference = 2 * pi * pieradius
+circumferencemsg = 'Your circle has a circumference of'
+print(circumferencemsg, circumference)
