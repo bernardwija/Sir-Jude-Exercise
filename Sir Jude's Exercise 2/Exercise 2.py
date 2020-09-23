d6029764a8a8f52c0b9712a00907e00a660bbd46
Bernard Wijaya <71361577+bernardwija@users.noreply.github.com>
2020-09-23 11:35:55 +0700

    Update Exercise 2.py

--- Sir Jude's Exercise 2/Exercise 2.py
+++ Sir Jude's Exercise 2/Exercise 2.py
@@ -1,5 +1,4 @@
 print('Welcome to Acceleration Counter')
-n = float(input('Please input the amount of force: '))
-g = float(input('Please input the amount of mass: '))
+n,g = eval(input('Please input the amount of force and mass separated by coma: '))
 a = n / g
 print('The Acceleration is',a)
