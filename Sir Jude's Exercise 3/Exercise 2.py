d0d4eb5bb43188e2434bfe5056cc9109ad52ee2a
Bernard Wijaya <71361577+bernardwija@users.noreply.github.com>
2020-10-08 10:48:42 +0700

    Update Exercise 2.py

--- Sir Jude's Exercise 3/Exercise 2.py
+++ Sir Jude's Exercise 3/Exercise 2.py
@@ -1,15 +1,20 @@
 print('Welcome to Weight Planet Counter!')
-print('Planet Selection : Earth, Jupiter')
+print('Planet Selection : Earth, Jupiter, Moon')
 a=input('Please Pick The Planet You Would Like To Choose: ')
-w=float(input('Please Input Your Body Weight: '))
+w = float(input('Please Input Your Body Weight: '))
 if a=='Earth':
-    print('Your Weight on Earth is', w)
+    def calc_weight_on_planet(w,g):
+        print('Your Weight on Earth is', w)
+    calc_weight_on_planet(w,9.8)
+elif a=='Jupiter':
+    def calc_weight_on_planet(w,g):
+        w1=w/g
+        w2=w1*23.1
+        print('Your Weight on Jupiter is',w2)
+    calc_weight_on_planet(w,9.8)
 else:
-    def calc_weight_on_planet():
-        f=w/9.8*23.1
-        return f
-    def main():
-        print(' ')
-        print('Your Weight on Jupiter is',calc_weight_on_planet())
-    main()
-
+    def calc_weight_on_planet(w,g):
+        w1=w/g
+        w2=w1*1.6
+        print('Your Weight on Moon is',w2)
+    calc_weight_on_planet(w,9.8)
