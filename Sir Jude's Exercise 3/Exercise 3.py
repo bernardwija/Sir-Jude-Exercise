a67acd66c0f4c9d0a647bfce87e5a3a14af7962a
Bernard Wijaya <71361577+bernardwija@users.noreply.github.com>
2020-10-08 10:49:26 +0700

    Update Exercise 3.py

--- Sir Jude's Exercise 3/Exercise 3.py
+++ Sir Jude's Exercise 3/Exercise 3.py
@@ -1,21 +1,22 @@
-print('Welcome to Atom Counter!')
+rint('Welcome to Atom Counter!')
 print('Substance List: Gold, Carbon, Hydrogen')
 a=input('Please Pick Which Substance You Would Like To Count: ')
 b=float(input('Please Input The Amount of '+a+' in grams: '))
 if a=='Gold':
-    def num_atoms():
-        g=b/196.97*6.022*10**23
-        return g
-    print('The Amount of Atoms in Your '+a+' is:',num_atoms())
+    def num_atoms(b,w):
+        m=b/w
+        g=m*6.022*10**23
+        print('The Amount of Atoms in Your '+a+' is:',g)
+    num_atoms(b,196.97)
 elif a=='Carbon':
-    def num_atoms():
-        g=b/12.001*6.022*10**23
-        return g
-    print('The Amount of Atoms in Your '+a+' is:',num_atoms())
+    def num_atoms(b,w):
+        m=b/w
+        c=m*6.022*10**23
+        print('The Amount of Atoms in Your '+a+' is:',c)
+    num_atoms(b,12.001)
 else:
-    def num_atoms():
-        g=b/1.008*6.022*10**23
-        return g
-    print('The Amount of Atoms in Your '+a+' is:',num_atoms())
-
-
+    def num_atoms(b,w):
+        m=b/w
+        h=m*6.022*10**23
+        print('The Amount of Atoms in Your '+a+' is:',h)
+    num_atoms(b,1.008)
