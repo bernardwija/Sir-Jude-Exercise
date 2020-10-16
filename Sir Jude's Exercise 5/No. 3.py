3750af7dd82b607ba47b9352ca2072325000119f
Bernard Wijaya <bernard.wijaya002@binus.ac.id>
2020-10-16 23:37:09 +0700

    Revert "Add files via upload"
    This reverts commit b74286142bceba6f0a3bbb3759b8e1a888dbf179.

--- Sir Jude's Exercise 5/No. 3.py
+++
@@ -1,6 +0,0 @@
-import calendar
-print('Welcome to Calendar Calculator!')
-y = int(input("Please input the year: "))
-m = int(input("Please input the month: "))
-print(calendar.month(y, m, w=0, l=0))
-print('Thankyou for using our service!')
