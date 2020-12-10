796f23aea2599b34b0f3b432e3c9634eab7f0e3b
Bernard Wijaya <71361577+bernardwija@users.noreply.github.com>
2020-12-11 01:55:17 +0700

    Update Person Exercise.py

--- Person Exercise/Person Exercise.py
+++ Person Exercise/Person Exercise.py
@@ -3,9 +3,9 @@ class Person:
         self.name=name
         self.address=address
     def getName(self):
-        return 'The name is',self.name
+        return self.name
     def getAddress(self):
-        return 'The Address is',self.address
+        return self.address
     def setAddress(self,address):
         self.address=address
     def toString(self):
@@ -15,8 +15,8 @@ class Student(Person):
     numCourses = 0
     grades = []
     courses = []
-    def __init__(self):
-        super().__init__('John','Boulevard Street')
+    def __init__(self,name,address):
+        super().__init__(name,address)
     def toString(self):
         return 'The name\'s' + self.name + 'and You live at' + self.address + '.'
     def addCourseGrade(self,course:list,grade:list):
@@ -26,26 +26,85 @@ class Student(Person):
             self.courses.append(i)
             self.numCourses +=1
     def printGrades(self):
-        return *self.courses, *self.grades
+        for a in range(len(self.courses)):
+            for b in range(len(self.grades)):
+                if a == b:
+                    print(f'{self.courses[a]} : {self.grades[b]}')
     def getAverageGrade(self):
-        return sum(self.grades)/self.numCourses
+        print('Average Grade:',sum(self.grades)/self.numCourses)
 
 class Teacher(Person):
     numCourses = 0
     courses = []
-    def __init__(self):
-        super().__init__('Jane','Lavender Street')
+    def __init__(self,name,address):
+        super().__init__(name,address)
     def toString(self):
         return 'The name\'s' + self.name + 'and You live at' + self.address + '.'
     def addCourse(self,course:list):
-        for i in course:
-            if i not in self.courses:
-                self.courses.append(course)
-                self.numCourses +=1
-            else:
-                return False
+        if course not in self.courses:
+            self.courses.append(course)
+            self.numCourses +=1
+        else:
+            return False
     def removeCourse(self,course:list):
-        for i in course:
-            if i in self.courses:
-                del i
-                self.numCourses -=1
+        if course in self.courses:
+            self.courses.remove(course)
+            self.numCourses -=1
+        else:
+            return False
+
+def stumenu():
+    return int(input("\nStudent Menu selection\n1. Add course and grades\n2. Print grades\n3. Average grade\n4. Back\nPlease input the service number:  "))
+
+def teamenu():
+    return int(input("\nTeacher Menu Selection:\n1. Add course\n2. Remove course\n3. Back\nPlease input the service number: "))
+
+def main():
+    print('Hello, Welcome to the School Website!\n')
+    inp = input("Menu Selection:\n1. Student\n2. Teacher\n3. Quit\nPlease input the service number: ")
+
+    while  inp!= '3':
+        if inp == '1':
+            name = input("\nInput your name: ")
+            address = input("Input your address: ")
+            student = Student(name, address)
+            print('\nWelcome',student.getName())
+            inpstu = stumenu()
+            while 0 < inpstu < 4:
+                if inpstu == 1:
+                    course = [str(x) for x in input("Input your courses in order separated by space: ").split()]
+                    grade = [int(x) for x in input("Input your grades in order separated by space: ").split()]
+                    student.addCourseGrade(course, grade)
+                    inpstu = stumenu()
+                if inpstu == 2:
+                    student.printGrades()
+                    inpstu = stumenu()
+                if inpstu == 3:
+                    student.getAverageGrade()
+                    inpstu = stumenu()
+            inp = input("Menu Selection:\n1. Student\n2. Teacher\n3. Quit\nPlease input the service number: ")
+
+        if inp == '2':
+            name = input("\nInput your name: ")
+            address = input("Input your address: ")
+            teacher = Teacher(name, address)
+            print('\nWelcome',teacher.getName())
+            inptea = teamenu()
+            while 0 < inptea < 3:
+                if inptea == '1':
+                    course = [str(x) for x in input("Input courses: ").split()]
+                    teacher.addCourse(course)
+                    inptea = teamenu()
+                if inptea == '2':
+                    course = [str(x) for x in input("Input courses to be removed: ").split()]
+                    teacher.removeCourse(course)
+                    inptea = teamenu()
+            inp = input("Menu Selection:\n1. Student\n2. Teacher\n3. Quit\nPlease input the service number: ")
+
+    print("Thankyou for using our service!")
+
+
+if __name__ == "__main__":
+    main()
+    
+    
